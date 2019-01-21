34779ee6b8a252be4b837919297d0ff15ad0f77e
Michel Bou khalil <michelboukhalil@Michels-MacBook-Pro.local>
2019-01-21 02:29:37 +0200

    1

---
+++ Todoey/Todoey/AppDelegate.swift
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.swift
+//  Todoey
+//
+//  Created by Michel Bou khalil on 12/4/18.
+//  Copyright © 2018 Michel Bou khalil. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        return true
+    }
+
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+       
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+               
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+
+
+}
+
---
+++ UserDefault.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let defaults = UserDefaults.standard
+
+defaults.set(0.24, forKey: "Volume")
+defaults.set(true, forKey: "MusicOn")
+defaults.set("Michel", forKey: "PlayerName")
+defaults.set(Date(), forKey: "AppLastOpenedByUser")
+
+ let array = [1,2,3]
+defaults.set(array, forKey: "myArray")
+let dictionary = ["name" : "Michel"]
+defaults.set(dictionary, forKey: "dictionaryKey")
+
+
+let volume = defaults.float(forKey: "Volume")
+let appLastOpen = defaults.object(forKey: "AppLastOpenedByUser")
+let myArray = defaults.array(forKey: "myArray") as! [Int]
+let myDictionary = defaults.dictionary(forKey: "dictionaryKey")
